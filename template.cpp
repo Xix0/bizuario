5fe73a85f950f874e6c7bac28b7003c408414ebb
Alexandre_Chiacchio <alexandrechiacchio30@gmail.com>
2021-09-14 18:10:31 -0300

    added more stuff to template

--- template.cpp
+++ template.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define first st;
-#define second sd;
+#define st first
+#define nd second
 //define push_back eh coisa de fraco
-#define push_back pb;
+#define pb push_back
 //eu sou fraco
+#define cl(x, v) memset((x), (v), sizeof(x))
+#define gcd(x,y) __gcd((x),(y))
 
 const int N = 1e5+5;
 const int MOD = 1e9+7;
-
+const long double EPS = 1e-9;
+const int PI = acos(-1.);
+const int INF = 0x3f3f3f3f;
 
 
 int main(){
+	//freopen("in", "r", stdin);
+	//freopen("out", "w", stdout);
 	return 0;
 }

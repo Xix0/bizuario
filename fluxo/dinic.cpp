163436753b002e38a98fefeb384afdb97bce6978
Alexandre_Chiacchio <alexandrechiacchio30@gmail.com>
2022-02-25 11:47:14 -0300

    formatted dinic/added ford-fulkerson and manacher

--- fluxo/dinic.cpp
+++ fluxo/dinic.cpp
@@ -5,8 +5,6 @@
 
 /**********************************************************************************
 * DINIC (FIND MAX FLOW / BIPARTITE MATCHING)                                      *
-* Description: Given a graph which represents a flow network where every edge has *
-* a capacity, find the maximum possible flow from s to t.                         *
 * Time complexity: O(EV^2)                                                        *
 * Usage: dinic()                                                                  *
 *        add_edge(from, to, capacity)                                             *
@@ -21,36 +19,37 @@
 using namespace std;
 
 const int N = 1e5 + 1, INF = 1e9;
-struct edge
-{
-	int v, c, f;
-};
+struct edge { int v, c, f; };
 
-int n, src, snk, h[N], ptr[N];
+int n, src = 0, snk = N-1, h[N], ptr[N];
 vector<edge> edgs;
 vector<int> g[N];
 
-void add_edge(int u, int v, int c)
-{
+void add_edge(int u, int v, int c) {
 	int k = edgs.size();
-	edgs.push_back({v, c, 0});
-	edgs.push_back({u, 0, 0});
+	edgs.push_back({ v, c, 0 });
+	edgs.push_back({ u, 0, 0 });
 	g[u].push_back(k);
 	g[v].push_back(k + 1);
 }
 
-bool bfs()
-{
+void clear() {
+	memset(h, 0, sizeof h);
+	memset(ptr, 0, sizeof ptr);
+	edgs.clear();
+	for (int i = 0; i < N; i++) g[i].clear();
+	src = 0;
+	snk = N - 1;
+}
+
+bool bfs() {
 	memset(h, 0, sizeof h);
 	queue<int> q;
 	h[src] = 1;
 	q.push(src);
-	while (!q.empty())
-	{
-		int u = q.front();
-		q.pop();
-		for (int i : g[u])
-		{
+	while (!q.empty()) {
+		int u = q.front(); q.pop();
+		for (int i : g[u]) {
 			int v = edgs[i].v;
 			if (!h[v] and edgs[i].f < edgs[i].c)
 				q.push(v), h[v] = h[u] + 1;
@@ -59,20 +58,15 @@ bool bfs()
 	return h[snk];
 }
 
-int dfs(int u, int flow)
-{
-	if (!flow or u == snk)
-		return flow;
-	for (int &i = ptr[u]; i < g[u].size(); ++i)
-	{
-		edge &dir = edgs[g[u][i]], &rev = edgs[g[u][i] ^ 1];
+int dfs(int u, int flow) {
+	if (!flow or u == snk) return flow;
+	for (int& i = ptr[u]; i < g[u].size(); ++i) {
+		edge& dir = edgs[g[u][i]], & rev = edgs[g[u][i] ^ 1];
 		int v = dir.v;
-		if (h[v] != h[u] + 1)
-			continue;
+		if (h[v] != h[u] + 1)  continue;
 		int inc = min(flow, dir.c - dir.f);
 		inc = dfs(v, inc);
-		if (inc)
-		{
+		if (inc) {
 			dir.f += inc, rev.f -= inc;
 			return inc;
 		}
@@ -80,20 +74,16 @@ int dfs(int u, int flow)
 	return 0;
 }
 
-int dinic()
-{
+int dinic() {
 	int flow = 0;
-	while (bfs())
-	{
+	while (bfs()) {
 		memset(ptr, 0, sizeof ptr);
-		while (int inc = dfs(src, INF))
-			flow += inc;
+		while (int inc = dfs(src, INF)) flow += inc;
 	}
 	return flow;
 }
 
-int main()
-{
-
+int main() {
+	clear();
 	return 0;
 }

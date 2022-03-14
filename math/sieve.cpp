2daa9f3fac682233edae8eea09f45aedc3480712
Alexandre_Chiacchio <alexandrechiacchio30@gmail.com>
2022-03-14 16:41:16 -0300

    corrected linear sieve, added rabin karp

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 const int N = 1e6;
 
 vector<int> primes;
@@ -19,36 +22,34 @@ void sieve(){
 void linear_sieve(){
 	bool pr[N];
 	memset(pr, 0, sizeof pr);
-	for(int i = 2; i<n; i++){
+	for(int i = 2; i<N; i++){
 		if(!pr[i]) primes.push_back(i);
-		for(int j = 0; j<primes.size() and i*prime[j]<N; j++){
+		for(int j = 0; j<primes.size() and i*primes[j]<N; j++){
 			pr[i * primes[j]] = true;
-			if(i%prime[j] == 0) break;
+			if(i%primes[j] == 0) break;
 		}
 	}
 }
 
 //let f be a multiplicative function to be precalculated - moubius for example
-vector<int> mobius(N);
+vector<int> m(N);
 
-void linear_sieve(){
-	bool pr[N];
-	memset(pr, 0, sizeof pr);
-	mobius[1] = 1;
-	for(int i = 2; i<N; i++){
-		if(!pr[i]){
-			primes.push_back(i);
-			mobius[i] = -1;
-		}
-		for(int j = 0; j<primes.size() and i*primes[j]<N; j++){
-			pr[i * primes[j]] = true;
-			if(i%(primes[j]*primes[j])==0) mobius[i] = 0;
-			mobius[i * primes[j]] = mobius[i]*mobius[primes[j]];
-		}
+void crivo(){
+	long long pr[N];
+	memset(pr, 0,sizeof pr);
+	for(long long i = 2; i<N; i++){
+		if(pr[i]) continue;
+		primes.push_back(i);
+		for(long long j = i; j<N; j+=i) pr[j]++;
 	}
+	for(long long i = 1; i<N; i++){
+		if(pr[i]&1) m[i] = -1;
+		else m[i] = 1;
+	}
+	for(long long i: primes) for(long long j = 1; j*i*i<N; j++) m[j*i*i] = 0;
 }
 
-//functions that are multiplicative:
+//functions that are multiplicative(but not completely multiplicative - only valid for coprimes):
 //Euler's totient function
 //mobius function
 //some others
--- string/rabin_karp.cpp
+++ string/rabin_karp.cpp
@@ -1,30 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 //ocoreencias de um padrão
-vi rabin_karp(string s, string t){
-    const int p = 31;
-    const int m = 1e9 + 9;
-    int S = s.size(), T = (s.size());
-    vector<ll> p_pot(max(S, T));
+vector<int> rabin_karp(string s, string t) {
+	const long long p = 31;
+	const long long m = 1e9 + 9;
+	int S = s.size(), T = t.size();
+	vector<long long> p_pot(max(S, T));
 
-    //precalcular as potencias
-    p_pot[0] = 1;
-    for(int i = 1; i < (int)p_pot.size(); i++) p_pot[i] = (p_pow[i-1] * p) % m;
-    
-    //calcular o hash pra cada prefixo de t
-    vector <ll> h(T + 1, 0);
-    for(int i = 0; i < T; i++) h[i+1] = (h[i] + (t[i] - 'a' + 1)*p_pot[i]) % m;
+	p_pot[0] = 1;
+	for (int i = 1; i < (int)p_pot.size(); i++) p_pot[i] = (p_pot[i - 1] * p) % m;
 
-    //calcular o hash para o padrão s que desejo procurar
-    for(int i = 0; i < S; i++) h_s = (h_s + (s[i] - 'a' + 1) * p_pot[i]) % m;
+	vector<long long> h(T + 1);
+	for (int i = 0; i < T; i++) h[i + 1] = (h[i] + (t[i] - 'a' + 1) * p_pot[i]) % m;
 
-    //vetor que guarda as posicoes das ocorrencias
-    vi occ;
+	long long h_s = 0;
+	for (int i = 0; i < (int)s.size(); i++) h_s = (h_s + (s[i] - 'a' + 1) * p_pot[i]) % m;
 
-    //procura as posicoes pra dar o padrão s dar match dentro da string t
-    for(int i = 0; i + S - 1 < T; i++){
-        ll cur_h = (h[i+S] - h[i] + m) % m;
-        //lembrar que a substring [i , i + S - 1], está shiftada de i posições
-        if(cur_h == h_s * p_pot[i] % m)
-            occ.pb(i);  
-    }
-    return occ;
+	vector<int> occ;
+
+	for (int i = 0; i + S - 1 < T; i++) {
+		long long cur_h = (h[i + S] - h[i] + m) % m;
+		if (cur_h == h_s * p_pot[i] % m) occ.push_back(i);
+	}
+
+	return occ;
 }
+
